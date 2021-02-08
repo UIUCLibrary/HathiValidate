0d18c60b276c3a1a0ce9dd83a0623af108ebaea3
henry <henryborchers@yahoo.com>
2021-02-08 12:40:14 -0600

    docstrings

--- hathi_validate/report.py
+++ hathi_validate/report.py
@@ -1,3 +1,5 @@
+"""Report generations tools."""
+
 import abc
 from typing import Iterator, IO, List, Generator
 import itertools
@@ -36,6 +38,16 @@ def _split_text_line_by_words(
 
 
 def make_point(message: str, width: int) -> Iterator[str]:
+    """Generate a bullet-point list.
+
+    Args:
+        message:
+        width: Column width
+
+    Yields:
+        Yields a new bullet-pointed line containing the message
+
+    """
     bullet = "* "
     for i, line in \
             enumerate(_split_text_line_by_words(message, width - len(bullet))):
@@ -47,11 +59,27 @@ def make_point(message: str, width: int) -> Iterator[str]:
 
 
 class ReportStringBuilder:
+    """Builder for creating a string report."""
+
     def __init__(self, results: List[result.Result]) -> None:
+        """Create a new String ReportStringBuilder object.
+
+        Args:
+            results: Results of a validation
+        """
         self.results = results
         self.header = "Validation Results"
 
     def build_string(self, width: int = 0) -> str:
+        """Create a new report as a string.
+
+        Args:
+            width: length of each line in the report
+
+        Returns:
+            Returns a multiline string based on the results given
+
+        """
         report_width = width if width > 0 else 80
 
         sorted_results = sorted(
@@ -79,6 +107,17 @@ class ReportStringBuilder:
                f"{main_spacer}"
 
     def get_warnings_section(self, grouped_results, report_width: int) -> str:
+        """Generate the section of the report containing the warnings.
+
+        Args:
+            grouped_results:
+            report_width:
+                Width of each line before a new line character is added.
+
+        Returns:
+            Returns the generated warnings section of the report as a string
+
+        """
         group_spacer = "-" * report_width
         if len(grouped_results) == 0:
             return "No validation errors detected.\n"
@@ -99,6 +138,18 @@ class ReportStringBuilder:
     def build_warning_message(group_name: str,
                               source_group: List[result.Result],
                               report_width: int) -> str:
+        """Build the warning message for each result provided.
+
+        Args:
+            group_name:
+            source_group:
+            report_width:
+                Width of each line before a new line character is added.
+
+        Returns:
+            Returns a new warning message based on the results.
+
+        """
         msg_list = []
         for msg in source_group:
             # if width > 0:
@@ -110,14 +161,37 @@ class ReportStringBuilder:
 
 
 def get_report_as_str(results: List[result.Result], width: int = 0) -> str:
+    """Generate a new report string from the results given.
+
+    Args:
+        results: results to generate a report from
+        width: Width of each line in the Report.
+
+    Returns:
+        Returns a new multiline report as a string
+
+    """
     builder = ReportStringBuilder(results)
     return builder.build_string(width)
 
 
 class AbsReport(metaclass=abc.ABCMeta):
+    """Base class for reporting reports.
+
+    For example: sending to the console or to a text file.
+    """
+
     @abc.abstractmethod
     def generate(self, results: List[result.Result]) -> None:
-        pass
+        """Generate the report and report it.
+
+        Notes:
+            This doesn't return anything.
+
+        Args:
+            results:
+
+        """
 
 
 class Report:
@@ -130,10 +204,23 @@ class Report:
 
 
 class ConsoleReport(AbsReport):
+    """Report to console."""
+
     def __init__(self, file: IO[str] = sys.stdout) -> None:
+        """Create a new object for reporting to the console.
+
+        Args:
+            file: such as stout or stderr
+        """
         self.file = file
 
     def generate(self, results: List[result.Result]) -> None:
+        """Generate the report and print it to the console.
+
+        Args:
+            results:
+
+        """
         sorted_results = sorted(
             results, key=lambda r: r.source if r.source is not None else ""
         )
@@ -148,10 +235,23 @@ class ConsoleReport(AbsReport):
 
 
 class LogReport(AbsReport):
+    """Report to a Python logger."""
+
     def __init__(self, logger: logging.Logger):
+        """Create a new object for reporting to a Python logger.
+
+        Args:
+            logger:
+        """
         self.logger = logger
 
     def generate(self, results: List[result.Result]) -> None:
+        """Generate the report and output to the Python logger.
+
+        Args:
+            results:
+
+        """
         sorted_results = sorted(
             results, key=lambda r: r.source if r.source is not None else ""
         )
@@ -177,10 +277,23 @@ class LogReport(AbsReport):
 
 
 class TextReport(AbsReport):
+    """Report to a file on a hard drive."""
+
     def __init__(self, file: str):
+        """Create a new object for sending reports to a file.
+
+        Args:
+            file: File path to save log information.
+        """
         self.file = file
 
     def generate(self, results: List[result.Result]) -> None:
+        """Generate the report and write it to a text file.
+
+        Args:
+            results:
+
+        """
         sorted_results = sorted(
             results, key=lambda r: r.source if r.source is not None else ""
         )
@@ -194,39 +307,96 @@ class TextReport(AbsReport):
 
 
 class AbsReporter(metaclass=abc.ABCMeta):
+    """Base class for reporter."""
+
     @abc.abstractmethod
     def report(self, report: str) -> None:
-        pass
+        """Send report."""
 
 
 class Reporter:
+    """Reporter strategy context."""
+
     def __init__(self, reporter_strategy: AbsReporter) -> None:
+        """Create a new strategy context object.
+
+        Args:
+            reporter_strategy: Strategy for reporting the data.
+        """
         self._strategy = reporter_strategy
 
     def report(self, report: str) -> None:
+        """Report the report.
+
+        Args:
+            report:
+
+        """
         self._strategy.report(report)
 
 
 class ConsoleReporter(AbsReporter):
+    """Report to console."""
+
     def __init__(self, file: IO[str] = sys.stdout) -> None:
+        """Create a new reporter object.
+
+        Args:
+            file: such as stderr or stdout
+
+        """
         self.file = file
 
     def report(self, report: str) -> None:
+        """Report the report.
+
+        Args:
+            report:
+
+        """
         print("\n\n{}".format(report), file=self.file)
 
 
 class FileOutputReporter(AbsReporter):
+    """Report to file."""
+
     def __init__(self, filename: str) -> None:
+        """Create a new reporter object.
+
+        Args:
+            filename: path to a file to save to
+
+        """
         self.filename = filename
 
     def report(self, report: str) -> None:
+        """Report the report.
+
+        Args:
+            report:
+
+        """
         with open(self.filename, "w", encoding="utf8") as write_file:
             write_file.write("{}\n".format(report))
 
 
 class LogReporter(AbsReporter):
+    """Report to Python logger."""
+
     def __init__(self, logger: logging.Logger) -> None:
+        """Create a new reporter object.
+
+        Args:
+            logger: Python logger
+
+        """
         self.logger = logger
 
     def report(self, report: str) -> None:
+        """Report the report.
+
+        Args:
+            report:
+
+        """
         self.logger.info("\n{}".format(report))
