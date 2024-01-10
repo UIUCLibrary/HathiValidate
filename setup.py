1bf2c028f6520967e7d7d7908f2ee340e82cd5e6
henry <henryborchers@yahoo.com>
2024-01-10 14:49:27 -0600

    remove pytest-runner from setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setup(
     ],
     package_data={'hathi_validate':["xsd/*.xsd", "py.typed"]},
     test_suite="tests",
-    setup_requires=['pytest-runner'],
     install_requires=[
         "lxml<5.1.0; sys_platform == 'darwin' and python_version == '3.8' and platform_machine == 'arm64'",
         "lxml; sys_platform != 'darwin' or python_version != '3.8' or platform_machine != 'arm64'",
@@ -19,7 +18,6 @@ setup(
         'importlib_resources;python_version<"3.9"',
                       ],
     tests_require=['pytest'],
-
     entry_points={
                  'console_scripts': ['hathivalidate=hathi_validate.cli:main']
              },
