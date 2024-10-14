13244f3d30ca2e58888cfcb23cec478656dc897d
Henry Borchers <henryborchers@yahoo.com>
2024-10-14 11:53:33 -0500

    build: move project settings to pyproject.toml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,17 +20,14 @@
 import os
 import re
 import sys
-
-try:
-    from setuptools.config.setupcfg import read_configuration
-except ModuleNotFoundError:
-    from setuptools.config import read_configuration
+import tomllib
 
 sys.path.insert(0, os.path.abspath('../..'))
 
 def get_project_metadata():
-    path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../setup.cfg"))
-    return read_configuration(path)["metadata"]
+    path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../pyproject.toml"))
+    with open(path, "rb") as f:
+        return tomllib.load(f)['project']
 
 metadata = get_project_metadata()
 # -- General configuration ------------------------------------------------
@@ -70,15 +67,15 @@ master_doc = 'index'
 
 # General information about the project.
 project = metadata['name']
-copyright = '2017, {}'.format(metadata['author'])
-author = metadata['author']
+copyright = '2017, {}'.format(metadata['authors'][0]['name'])
+author = metadata['authors'][0]['name']
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
 # The short X.Y version.
-version_extractor = re.compile("\d+[.]\d+[.]\d+")
+version_extractor = re.compile(r"\d+[.]\d+[.]\d+")
 version = version_extractor.search(metadata["version"]).group(0)
 # The full version, including alpha/beta/rc tags.
 release = metadata["version"]
--- setup.py
+++ setup.py
@@ -1,24 +1,3 @@
-import os
-
-
 from setuptools import setup
 
-
-setup(
-    packages=[
-        'hathi_validate',
-        'hathi_validate.xsd',
-    ],
-    package_data={'hathi_validate':["xsd/*.xsd", "py.typed"]},
-    test_suite="tests",
-    install_requires=[
-        "lxml<5.1.0; sys_platform == 'darwin' and python_version == '3.8' and platform_machine == 'arm64'",
-        "lxml; sys_platform != 'darwin' or python_version != '3.8' or platform_machine != 'arm64'",
-        "PyYAML",
-        'importlib_resources;python_version<"3.9"',
-                      ],
-    tests_require=['pytest'],
-    entry_points={
-                 'console_scripts': ['hathivalidate=hathi_validate.cli:main']
-             },
-)
+setup()
