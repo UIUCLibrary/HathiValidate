71554eee07c94ed34370214be84e5365b94e47ab
Henry Borchers <henryborchers@yahoo.com>
2024-12-19 12:56:48 -0600

    ci: jenkins docker containers on windows only cache UV_PYTHON_INSTALL_DIR path

--- vars/runJenkinsPipeline.groovy
+++ vars/runJenkinsPipeline.groovy
@@ -515,7 +515,8 @@ def call(){
                                     script{
                                         def envs = []
                                         node('docker && windows'){
-                                            docker.image('python').inside('--mount source=python-tmp-hathivalidate,target=C:\\Users\\ContainerUser\\Documents'){
+                                            docker.image('python').inside("--mount type=volume,source=uv_python_install_dir,target=${env.UV_PYTHON_INSTALL_DIR}"){
+
                                                 try{
                                                     checkout scm
                                                     bat(script: 'python -m venv venv && venv\\Scripts\\pip install uv')
@@ -542,7 +543,7 @@ def call(){
                                                     "Tox Environment: ${toxEnv}",
                                                     {
                                                         node('docker && windows'){
-                                                            docker.image('python').inside('--mount source=python-tmp-hathivalidate,target=C:\\Users\\ContainerUser\\Documents'){
+                                                            docker.image('python').inside("--mount type=volume,source=uv_python_install_dir,target=${env.UV_PYTHON_INSTALL_DIR}"){
                                                                 checkout scm
                                                                 try{
                                                                     bat(label: 'Install uv',
