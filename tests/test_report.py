e63f5a614a138fbd148813cc4e56e8fcb1ec5d73
Henry Borchers <henryborchers@yahoo.com>
2025-06-30 14:39:49 -0500

    added tests for report.Reporter, report.ConsoleReporter, & result.Result

--- tests/test_report.py
+++ tests/test_report.py
@@ -1,3 +1,6 @@
+import io
+from unittest.mock import Mock
+
 from hathi_validate import report, result
 
 
@@ -11,3 +14,18 @@ def test_report_as_str_is_a_string_message():
     assert \
         isinstance(final_report, str) and \
         error_result.message in final_report
+
+
+class TestConsoleReporter:
+    def test_report_to_stream(self):
+        stream = io.StringIO()
+        stream_reporter = report.ConsoleReporter(stream)
+        stream_reporter.report("spam")
+        assert "spam" in stream.getvalue()
+
+class TestReporter:
+    def test_report_calls_strategy(self):
+        reporter_strategy = Mock(spec=report.AbsReporter)
+        reporter = report.Reporter(reporter_strategy=reporter_strategy)
+        reporter.report("spam")
+        reporter_strategy.report.assert_called_once_with("spam")
